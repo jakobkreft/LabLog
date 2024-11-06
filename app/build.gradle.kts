38bcf22c1103b698f497b6b95b8fdba355ca2026
Jakob Kreft <jakob.kreft@student.kuleuven.be>
2024-11-07 00:50:15 +0100

    small ui fix before new release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "si.uni_lj.fe.lablog"
         minSdk = 29
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
