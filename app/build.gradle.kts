943d4bfc104f062cfdb09e0fb66d4e126fe82894
Jakob Kreft <jakob.kreft@student.kuleuven.be>
2024-11-19 01:53:38 +0100

    new release version 7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "si.uni_lj.fe.lablog"
         minSdk = 29
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.5"
+        versionCode = 7
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
